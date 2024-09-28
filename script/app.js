f79c8c52f46991ff5c716b53b971ac4fc7875288
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 19:52:41 +0400

    show success and error messages

--- script/app.js
+++ script/app.js
@@ -2,6 +2,15 @@ function checkEmail() {
   const emailInput = document.getElementById("email-input");
   const emailValue = emailInput.value.trim();
   const errorMessage = document.getElementById("error-message");
+
+  if (!isValidEmail(emailValue)) {
+    errorMessage.textContent = "Invalid email address";
+  } else {
+    emailInput.value = "Success!";
+    setTimeout(() => {
+      emailInput.value = "";
+    }, 2000);
+  }
 }
 
 function isValidEmail(email) {
