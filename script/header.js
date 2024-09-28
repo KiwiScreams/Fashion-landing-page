61f4cd74e7dd17d42345283f5152cf3d353e8b01
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 20:56:36 +0400

    fix header scrolling

--- script/header.js
+++ script/header.js
@@ -3,7 +3,7 @@ const nav = document.getElementById("navbar");
 window.addEventListener("scroll", function () {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop > lastScrollTop) {
-    nav.style.top = "-80px";
+    nav.style.top = "-160px";
   } else {
     nav.style.top = "0px";
   }
