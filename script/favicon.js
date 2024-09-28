6f32a1912b46202058f0e5aee3c16be734510a57
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 20:29:42 +0400

    change favicon depending on dark light mode

--- script/favicon.js
+++ script/favicon.js
@@ -1,3 +1,9 @@
 const faviconTag = document.getElementById("faviconTag");
 const isDark = window.matchMedia('(prefers-color-scheme: dark)');
-console.log(isDark.matches)
+console.log(isDark.matches);
+const changeFavicon = () =>
+{
+    if(isDark.matches) faviconTag.href="./assets/images/logo.svg";
+    else faviconTag.href="./assets/images/FASHION.svg";
+}
+changeFavicon();
