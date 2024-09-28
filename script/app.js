65076405e0de97cf2a398bc148c47017124dcefa
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 19:52:20 +0400

    create values

--- script/app.js
+++ script/app.js
@@ -1,3 +1,9 @@
+function checkEmail() {
+  const emailInput = document.getElementById("email-input");
+  const emailValue = emailInput.value.trim();
+  const errorMessage = document.getElementById("error-message");
+}
+
 function isValidEmail(email) {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
