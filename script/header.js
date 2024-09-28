dc3e2e6c4b72cb477721b4eecfc5d2e882db0ed8
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 20:53:46 +0400

    header scroll to top

--- script/header.js
+++ script/header.js
@@ -1,2 +1,11 @@
 let lastScrollTop = 0;
 const nav = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    nav.style.top = "-80px";
+  } else {
+    nav.style.top = "0px";
+  }
+  lastScrollTop = scrollTop;
+});
