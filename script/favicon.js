318dd310dd950e6cf5c19ee8cc7986afa6618bc6
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 20:32:37 +0400

    delete console

--- script/favicon.js
+++ script/favicon.js
@@ -1,6 +1,5 @@
 const faviconTag = document.getElementById("faviconTag");
 const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-console.log(isDark.matches);
 const changeFavicon = () => {
   if (isDark.matches) faviconTag.href = "./assets/images/light.svg";
   else faviconTag.href = "./assets/images/logo.svg";
