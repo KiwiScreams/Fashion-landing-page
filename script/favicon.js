912978f5e9bc80c5bba40840a295a34e0b1af77e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-28 20:32:20 +0400

    favicon icons

--- script/favicon.js
+++ script/favicon.js
@@ -1,9 +1,8 @@
 const faviconTag = document.getElementById("faviconTag");
-const isDark = window.matchMedia('(prefers-color-scheme: dark)');
+const isDark = window.matchMedia("(prefers-color-scheme: dark)");
 console.log(isDark.matches);
-const changeFavicon = () =>
-{
-    if(isDark.matches) faviconTag.href="./assets/images/logo.svg";
-    else faviconTag.href="./assets/images/FASHION.svg";
-}
+const changeFavicon = () => {
+  if (isDark.matches) faviconTag.href = "./assets/images/light.svg";
+  else faviconTag.href = "./assets/images/logo.svg";
+};
 changeFavicon();
